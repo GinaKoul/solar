605aea1db3158dee78ea5376d6833104cbbafa97
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2023-05-07 14:28:23 +0300

    Change arrow direction on click

--- javascript.js
+++ javascript.js
@@ -7,4 +7,7 @@ creditsB.addEventListener('click',credits);
 function credits(){
     const credit = document.querySelector(".end");
     credit.classList.toggle("credit");
+    const arrow = document.querySelector(".arrow");
+    arrow.classList.toggle("down");
+    arrow.classList.toggle("up");
 }
